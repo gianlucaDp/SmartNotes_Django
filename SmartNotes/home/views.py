4bbb31ce282a96354fb779a25acc933748526677
Gianluca De Prisco <mistergdp@gmail.com>
2022-03-03 17:54:32 +0100

    Base commit
    Added base website, added home , superuser and authenticated webpage

---
+++ SmartNotes/home/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from datetime import datetime
+from django.contrib.auth.decorators import login_required
+
+
+def home(request):
+    return render(request, "home/home.html", {'today': datetime.today()})
+
+
+@login_required(login_url='/admin')
+def authorized(request):
+    return render(request, "home/authorized.html", {})
