a669abf7e8e65ddd4598b8b7a6fea3fdfdea7fb9
Gianluca De Prisco <mistergdp@gmail.com>
2022-03-29 13:52:57 +0200

    Added user management
    - Added pages to signup, login and logout
    - Now only notes for the logged user are visible

--- SmartNotes/home/urls.py
+++ SmartNotes/home/urls.py
@@ -3,7 +3,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view()),
-    path('authorized', views.AuthorizedView.as_view())
+    path('', views.HomeView.as_view(), name='home'),
+    path('authorized', views.AuthorizedView.as_view(), name='credential_check'),
+    path('login', views.LoginInterfaceView.as_view(), name='login'),
+    path('logout', views.LogoutInterfaceView.as_view(), name='logout'),
+    path('signup', views.SignupView.as_view(), name='signup'),
 
 ]
--- SmartNotes/notes/views.py
+++ SmartNotes/notes/views.py
@@ -1,15 +1,21 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
 from .forms import NotesForm
 from .models import Note
 from django.views.generic import ListView, DetailView, CreateView
 from django.views.generic.edit import UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class NotesListView(ListView):
+class NotesListView(LoginRequiredMixin, ListView):
     model = Note
     context_object_name = "notes"
     template_name = "notes/notes_list.html"  # not necessary if we follow name convention
+    login_url = "/login"
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
 
 
 class NotesCreateView(CreateView):
@@ -17,6 +23,12 @@ class NotesCreateView(CreateView):
     form_class = NotesForm
     success_url = "/notes"
     template_name = "notes/notes_form.html"
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
 
 
 class NotesUpdateView(UpdateView):
